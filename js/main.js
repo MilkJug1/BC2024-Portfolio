ce27d25cc4b49bb9fcc7aafbbb4972786d530cd9
MilkJug <michael.sagan.crawford@gmail.com>
2024-06-28 21:19:53 -0400

    Template carosuel

--- js/main.js
+++ js/main.js
@@ -21,3 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', adjustOpacity);
     adjustOpacity(); // Initial check in case the elements are already in view on load
 });
+
+
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+  list.scrollLeft += (itemWidth + padding)
+})
